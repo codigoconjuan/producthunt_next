6db8abbec92daf1d81dd1afa5af989c5bc3b0e0d
JuanDevWP <blackmaxxgdl18@hotmail.com>
2020-01-04 14:39:38 -0600

    agregando css global

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import Link from 'next/link';
 import Header from './Header';
+import { Global, css } from '@emotion/core';
 
 const Layout = props => {
     return ( 
         <>
+            <Global 
+                styles={css`
+                    :root {
+                        --gris: #3d3d3d;
+                        --gris2: #6F6F6F;
+                        --naranja: #DA552F;
+                    }
+
+                    html {
+                        font-size: 62.5%;
+                        box-sizing: border-box;
+                    }
+                    *, *:before, *:after {
+                        box-sizing: inherit;
+                    }
+                    body {
+                        font-size: 1.6rem; 
+                        line-height: 1.5;
+                    }
+                    h1, h2, h3 {
+                        margin: 0 0 2rem 0;
+                        line-height: 1.5;
+                    }
+
+                    ul {
+                        list-style: none;
+                        margin: 0;
+                        padding:0;
+                    }
+                    a {
+                        text-decoration: none;
+                    }
+                `}
+            />
             <Header />
 
             <main>

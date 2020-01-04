c3747d0ca179239dccf0851ab4d6bbd2fe5c117e
JuanDevWP <blackmaxxgdl18@hotmail.com>
2020-01-04 14:59:33 -0600

    CSS al header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -2,13 +2,42 @@ import React from 'react';
 import Buscar from '../ui/Buscar';
 import Navegacion from './Navegacion';
 import Link from 'next/link';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+const ContenedorHeader = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width:768px) {
+        display: flex;
+        justify-content: space-between;
+    }
+`;
+
+const Logo = styled.p`
+    color: var(--naranja);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+`;
 
 const Header = () => {
     return ( 
-        <header>
-            <div>
+        <header
+            css={css`
+                border-bottom: 2px solid var(--gris3);
+                padding: 1rem 0;
+            `}
+        >
+            <ContenedorHeader>
                 <div>
-                    <p>P</p>
+                    <Link href="/">
+                        <Logo>P</Logo>
+                    </Link>
+                    
 
                     <Buscar />
 
@@ -23,7 +52,7 @@ const Header = () => {
                     <Link href="/">Login</Link>
                     <Link href="/">Crear Cuenta</Link>
                 </div>
-            </div>
+            </ContenedorHeader>
         </header>
      );
 }

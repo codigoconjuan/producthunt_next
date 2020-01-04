30f9136bd13cbec30594d034fc268ae56064a487
JuanDevWP <blackmaxxgdl18@hotmail.com>
2020-01-04 14:06:34 -0600

    agregando emotion

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import Head from 'next/head'
 
+
 const Home = () => (
   <div>
       <h1>Inicio</h1>
-
-      <style jsx>{`
-          h1 {
-            color: red;
-          }
-      `}</style>
   </div>
 )
 

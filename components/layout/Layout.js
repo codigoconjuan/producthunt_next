8c1fa5918c3d896af20dd231f3ede9966e06df26
JuanDevWP <blackmaxxgdl18@hotmail.com>
2020-01-04 14:17:49 -0600

    routing en nextjs

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Layout = props => {
     return ( 
         <>
             <h1>Header</h1>
-            
+
+            <nav>
+                <Link href="/">Inicio</Link>
+                <Link href="/nosotros">Nosotros</Link>
+            </nav>
+
             <main>
                 {props.children}
             </main>
---
+++ pages/nosotros.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout/Layout';
+
+
+const Nosotros = () => (
+  <div>
+    <Layout>
+      <h1>Nosotros</h1>
+    </Layout>
+  </div>
+)
+
+export default Nosotros

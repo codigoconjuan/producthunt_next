f425601221e8fe5613dc13a869cb3aa95f186e53
JuanDevWP <blackmaxxgdl18@hotmail.com>
2020-01-04 15:10:24 -0600

    CSS a los botones

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Buscar from '../ui/Buscar';
-import Navegacion from './Navegacion';
 import Link from 'next/link';
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
+import Buscar from '../ui/Buscar';
+import Navegacion from './Navegacion';
+import Boton from '../ui/Boton';
 
 const ContenedorHeader = styled.div`
     max-width: 1200px;
@@ -25,6 +26,10 @@ const Logo = styled.p`
 `;
 
 const Header = () => {
+
+
+    const usuario = true;
+
     return ( 
         <header
             css={css`
@@ -44,13 +49,35 @@ const Header = () => {
                     <Navegacion />
                 </div>
 
-                <div>
-                    <p>Hola: Juan</p>
-
-                    <button type="button">Cerrar Sesión</button>
-                    
-                    <Link href="/">Login</Link>
-                    <Link href="/">Crear Cuenta</Link>
+                <div
+                    css={css`
+                        display: flex;
+                        align-items: center;
+                    `}
+                >
+                    { usuario ? (
+                        <>
+                            <p
+                                css={css`
+                                    margin-right: 2rem;
+                                `}
+                            >Hola: Juan</p>
+                            <Boton
+                                bgColor="true"
+                            >Cerrar Sesión</Boton>
+                        </>
+                    ) : (
+                        <>
+                            <Link href="/">
+                                <Boton
+                                    bgColor="true"
+                                >Login</Boton>
+                            </Link>
+                            <Link href="/">
+                                <Boton>Crear Cuenta</Boton>
+                            </Link>
+                        </>
+                    ) }
                 </div>
             </ContenedorHeader>
         </header>

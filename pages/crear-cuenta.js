16bf2ea3f60d376d6778cb8285d0059e11c5af39
JuanDevWP <blackmaxxgdl18@hotmail.com>
2020-01-04 19:22:23 -0600

    primeros pasos con crear cuenta

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -5,7 +5,45 @@ import Layout from '../components/layout/Layout';
 const CrearCuenta = () => (
   <div>
     <Layout>
-      <h1>Crear Cuenta</h1>
+      <>
+        <h1>Crear Cuenta</h1>
+        <form>
+            <div>
+                <label htmlFor="nombre">Nombre</label>
+                <input 
+                    type="text"
+                    id="nombre"
+                    placeholder="Tu Nombre"
+                    name="nombre"
+                />
+            </div>
+
+            <div>
+                <label htmlFor="email">Email</label>
+                <input 
+                    type="email"
+                    id="email"
+                    placeholder="Tu Email"
+                    name="email"
+                />
+            </div>
+
+            <div>
+                <label htmlFor="password">Password</label>
+                <input 
+                    type="password"
+                    id="password"
+                    placeholder="Tu Password"
+                    name="password"
+                />
+            </div>
+
+            <input 
+              type="submit"
+              value="Crear Cuenta"
+            />
+        </form>
+      </>
     </Layout>
   </div>
 )

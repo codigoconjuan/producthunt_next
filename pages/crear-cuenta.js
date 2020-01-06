88d6686321009ff34fbcaf27f4303a69bdafeb5a
JuanDevWP <blackmaxxgdl18@hotmail.com>
2020-01-06 12:45:00 -0600

    finalizando crear cuenta

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { css } from '@emotion/core';
+import Router from 'next/router';
 import Layout from '../components/layout/Layout';
 import { Formulario, Campo, InputSubmit, Error } from '../components/ui/Formulario';
 
@@ -17,6 +18,8 @@ const STATE_INICIAL = {
  
 const CrearCuenta = () => {
 
+  const [ error, guardarError] = useState(false);
+
   const { valores, errores, handleSubmit, handleChange, handleBlur } = useValidacion(STATE_INICIAL, validarCrearCuenta, crearCuenta);
 
   const { nombre, email, password } = valores;
@@ -24,9 +27,10 @@ const CrearCuenta = () => {
   async function crearCuenta() {
     try {
       await firebase.registrar(nombre, email, password);
-
+      Router.push('/');
     } catch (error) {
       console.error('Hubo un error al crear el usuario ', error.message);
+      guardarError(error.message);
     }
   }
 
@@ -87,6 +91,8 @@ const CrearCuenta = () => {
                   />
               </Campo>
               {errores.password && <Error>{errores.password}</Error> }
+
+              {error && <Error>{error} </Error>}
   
               <InputSubmit 
                 type="submit"

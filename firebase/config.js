71cebb69cc5e61337bde59601c17d6473e3c3076
JuanDevWP <blackmaxxgdl18@hotmail.com>
2020-01-06 12:12:00 -0600

    agregando firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB-JONvKYwSdY_rFydvlDUQSi5-2ZdIOXQ",
+    authDomain: "product-hunt-1e21e.firebaseapp.com",
+    databaseURL: "https://product-hunt-1e21e.firebaseio.com",
+    projectId: "product-hunt-1e21e",
+    storageBucket: "product-hunt-1e21e.appspot.com",
+    messagingSenderId: "728310011880",
+    appId: "1:728310011880:web:38f7a45b24a8cf7a2a765d"
+};
+
+export default firebaseConfig;

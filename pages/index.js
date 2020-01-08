c74284a4466f67f5f11ba4420efc76847735cc65
JuanDevWP <blackmaxxgdl18@hotmail.com>
2020-01-07 18:35:29 -0600

    mostrando registros en home

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import Layout from '../components/layout/Layout';
+import { FirebaseContext } from '../firebase';
 
 
-const Home = () => (
-  <div>
-    <Layout>
-      <h1>Inicio</h1>
-    </Layout>
-  </div>
-)
+const Home = () => {
+
+  const [productos, guardarProductos] = useState([]);
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+        firebase.db.collection('productos').orderBy('creado', 'desc').onSnapshot(manejarSnapshot)
+    }
+    obtenerProductos();
+  }, []);
+
+  function manejarSnapshot(snapshot) {
+      const productos = snapshot.docs.map(doc => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        }
+      });
+
+      guardarProductos(productos);
+  }
+
+  return (
+    <div>
+      <Layout>
+        <h1>Inicio</h1>
+      </Layout>
+    </div>
+  )
+}
 
 export default Home

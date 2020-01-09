84ecc8045c602f878a740498bc12916a25d7ca22
JuanDevWP <blackmaxxgdl18@hotmail.com>
2020-01-09 17:06:27 -0600

    mostrando información de producto

--- components/layout/DetallesProducto.js
+++ components/layout/DetallesProducto.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import { es } from 'date-fns/locale'
+
+const Imagen = styled.img`
+    width: 200px;
+`;
 
 const DetallesProducto = ({producto}) => {
 
@@ -9,16 +16,26 @@ const DetallesProducto = ({producto}) => {
         <li>
             <div>
                 <div>
-
+                    <Imagen src={urlimagen}  />
                 </div>
 
                 <div>
                     <h1>{nombre}</h1>
+
+                    <p>{descripcion}</p>
+
+                    <div>
+                        <img src="/static/img/comentario.png" />
+                        <p>{comentarios.length} Comentarios</p>
+                    </div>
+
+                    <p>Publicado hace: { formatDistanceToNow( new Date(creado), {locale: es} )} </p>
                 </div>
             </div>
 
             <div>
-
+                <div> &#9650; </div>
+                <p>{votos}</p>
             </div>
         </li>
      );

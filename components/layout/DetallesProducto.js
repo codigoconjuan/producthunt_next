5ce765437cdbfc9a95ffbbaa0a3ae5255f11def1
JuanDevWP <blackmaxxgdl18@hotmail.com>
2020-01-09 17:20:11 -0600

    css a productos en home

--- components/layout/DetallesProducto.js
+++ components/layout/DetallesProducto.js
@@ -1,10 +1,82 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
-import { es } from 'date-fns/locale'
+import { es } from 'date-fns/locale';
+
+const Producto = styled.li`
+    padding: 4rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #e1e1e1;
+`;
+const DescripcionProducto = styled.div`
+    flex: 0 1 600px;
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    column-gap: 2rem;
+`;
+
+const Titulo = styled.a`
+    font-size: 2rem;
+    font-weight: bold;
+    margin: 0;
+
+    :hover {
+        cursor: pointer;
+    }
+`;
+
+const TextoDescripcion = styled.p`
+    font-size: 1.6rem;
+    margin: 0;
+    color: #888;
+`
+
+const Comentarios = styled.div`
+    margin-top: 2rem;
+    display: flex;
+    align-items: center;
+    div {
+        display: flex;
+        align-items: center;
+        border: 1px solid #e1e1e1;
+        padding: .3rem 1rem;
+        margin-right: 2rem;
+    }
+    img {
+        width: 2rem;
+        margin-right: 2rem;
+    }
+    p {
+        font-size: 1.6rem;
+        margin-right: 1rem;
+        font-weight: 700;
+        &:last-of-type {
+            margin: 0;
+        }
+    }
+`;
 
 const Imagen = styled.img`
     width: 200px;
+`;
+
+const Votos = styled.div`
+    flex: 0 0 auto;
+    text-align: center; 
+    border: 1px solid #e1e1e1;
+    padding: 1rem 3rem;
+
+    div {
+        font-size: 2rem;
+    }
+
+    p {
+        margin: 0;
+        font-size: 2rem;
+        font-weight: 700;
+    }
 `;
 
 const DetallesProducto = ({producto}) => {
@@ -13,31 +85,30 @@ const DetallesProducto = ({producto}) => {
 
 
     return ( 
-        <li>
-            <div>
+        <Producto>
+            <DescripcionProducto>
                 <div>
                     <Imagen src={urlimagen}  />
                 </div>
 
                 <div>
-                    <h1>{nombre}</h1>
-
-                    <p>{descripcion}</p>
-
-                    <div>
-                        <img src="/static/img/comentario.png" />
-                        <p>{comentarios.length} Comentarios</p>
-                    </div>
-
+                    <Titulo>{nombre}</Titulo>
+                    <TextoDescripcion>{descripcion}</TextoDescripcion>
+                    <Comentarios>
+                        <div>
+                            <img src="/static/img/comentario.png" />
+                            <p>{comentarios.length} Comentarios</p>
+                        </div>
+                    </Comentarios>
                     <p>Publicado hace: { formatDistanceToNow( new Date(creado), {locale: es} )} </p>
                 </div>
-            </div>
+            </DescripcionProducto>
 
-            <div>
+            <Votos>
                 <div> &#9650; </div>
                 <p>{votos}</p>
-            </div>
-        </li>
+            </Votos>
+        </Producto>
      );
 }
  

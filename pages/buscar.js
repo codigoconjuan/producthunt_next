64ff345688d7c3e5e0e999b42c8c5aa3151c5300
JuanDevWP <blackmaxxgdl18@hotmail.com>
2020-01-10 19:28:58 -0600

    finalizando el proyecto

--- pages/buscar.js
+++ pages/buscar.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../components/layout/Layout';
 import { useRouter } from 'next/router';
+import DetallesProducto from '../components/layout/DetallesProducto';
+import useProductos from '../hooks/useProductos';
 
 const Buscar = () => {
 
   const router = useRouter();
   const { query: { q }} = router;
-  console.log(q);
+
+  // Todos los productos
+  const { productos } = useProductos('creado');
+  const [ resultado, guardarResultado ] = useState([]);
+
+  useEffect(() => {
+      const busqueda = q.toLowerCase();
+      const filtro =  productos.filter(producto => {
+        return (
+          producto.nombre.toLowerCase().includes(busqueda) || 
+          producto.descripcion.toLowerCase().includes(busqueda)
+        )
+      });
+      guardarResultado(filtro);
+      
+  }, [ q, productos ]);
 
 
   return (
     <div>
       <Layout>
-        <h1>Buscar</h1>
+        <div className="listado-productos">
+            <div className="contenedor">
+              <ul className="bg-white">
+                  {resultado.map(producto => (
+                      <DetallesProducto
+                          key={producto.id}
+                          producto={producto}
+                      />
+                  ))}
+              </ul>
+            </div>
+        </div>
       </Layout>
     </div>
   )
